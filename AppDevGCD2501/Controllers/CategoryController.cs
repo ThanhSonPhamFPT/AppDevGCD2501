ef3daa67ec85fa43f2192f520942131a59cd7a98
Pham Thanh Son <sonpt28@fpt.edu.vn>
2025-03-26 11:04:24 +0700

    Add project files.

---
+++ AppDevGCD2501/Controllers/CategoryController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppDevGCD2501.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Hello()
+        {
+            return View();
+        }
+    }
+}
---
+++ AppDevGCD2501/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppDevGCD2501.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
