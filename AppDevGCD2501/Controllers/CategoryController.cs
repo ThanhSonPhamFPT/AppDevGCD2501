f0a0c5148b146c37070f832b8ac66e64a0541f72
Pham Thanh Son <sonpt28@fpt.edu.vn>
2025-04-02 11:23:59 +0700

    CRUD application for Category

--- AppDevGCD2501/Controllers/CategoryController.cs
+++ AppDevGCD2501/Controllers/CategoryController.cs
@@ -1,14 +1,73 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AppDevGCD2501.Data;
+using AppDevGCD2501.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AppDevGCD2501.Controllers
 {
     public class CategoryController : Controller
     {
+        private ApplicationDBContext dbContext;
+        public CategoryController(ApplicationDBContext _dbContext)
+        {
+            dbContext = _dbContext;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Category> listCategory = dbContext.Categories.ToList();
+            return View(listCategory);
         }
-        public IActionResult Hello()
+		public IActionResult Add()
+		{
+			return View();
+		}
+        [HttpPost]
+		public IActionResult Add(Category category)
+		{
+            if (ModelState.IsValid)
+            {
+				dbContext.Categories.Add(category);
+				dbContext.SaveChanges();
+				return RedirectToAction("Index");
+			}
+            return View();
+		}
+		public IActionResult Edit(int id)
+		{
+            Category category = dbContext.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+			return View(category);
+		}
+		[HttpPost]
+		public IActionResult Edit(Category category)
+		{
+			if (ModelState.IsValid)
+			{
+				dbContext.Categories.Update(category);
+				dbContext.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+		public IActionResult Delete(int id)
+		{
+			Category category = dbContext.Categories.Find(id);
+			if (category == null)
+			{
+				return NotFound();
+			}
+			return View(category);
+		}
+		[HttpPost]
+		public IActionResult Delete(Category category)
+		{			
+				dbContext.Categories.Remove(category);
+				dbContext.SaveChanges();
+				return RedirectToAction("Index");
+		}
+		public IActionResult Hello()
         {
             return View();
         }
